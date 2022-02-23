d70bce053e66df245f9fdd15304ec26a7a9861c1
brackenBYUI <77087966+brackenBYUI@users.noreply.github.com>
2022-02-23 12:32:54 -0700

    Week 7 update

--- cms/src/app/contacts/contact.service.ts
+++ cms/src/app/contacts/contact.service.ts
@@ -8,11 +8,13 @@ import {MOCKCONTACTS} from './MOCKCONTACTS';
 })
 export class ContactService {
   contactChangedEvent = new Subject<Contact[]>();
+  maxContactId: string
 
   private contacts: Contact[] = [];
 
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
    }
 
    getContacts(): Contact[] {
@@ -40,4 +42,45 @@ export class ContactService {
      this.contacts.splice(pos, 1);
      this.contactChangedEvent.next(this.contacts.slice());
    }
+
+   getMaxId(): string {
+    let maxId = 0
+    let currentId = 0
+  
+    for(let document of this.contacts) {
+        currentId = +document.id;
+  
+        if ( currentId > maxId ){
+          maxId = currentId
+        }
+   }
+  
+    return maxId.toString()
+  }
+
+  addDocument(newContact: Contact) {
+    if (!newContact) {
+      return
+    }
+    let id = parseInt(this.maxContactId) + 1
+    this.maxContactId = id.toString()
+    newContact.id = this.maxContactId
+    this.contacts.push(newContact)
+    this.contactChangedEvent.next(this.contacts.slice())
+  }
+
+  updateDocument(originalContact: Contact, newContact: Contact) {
+    if (!newContact || !originalContact) {
+      return
+    }
+  
+    let pos = this.contacts.indexOf(originalContact)
+    if (pos < 0) {
+      return
+    } 
+        
+    newContact.id = originalContact.id
+    this.contacts[pos] = newContact
+    this.contactChangedEvent.next(this.contacts.slice())
+  }
 }
